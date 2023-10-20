55dd7cad806708731477d11182cc0a9f0d12e7f3
danielbatres <danielbatresuser@gmail.com>
2023-10-20 12:13:37 -0600

    Casting

---
+++ casting.php
@@ -0,0 +1,23 @@
+<?php
+
+$number = "5";
+
+var_dump($number);
+
+$number = (int) $number;
+
+var_dump($number);
+
+$days = 5.89;
+
+$days = (int) $days;
+
+var_dump($days);
+
+$flag = 0;
+
+$flag = (bool) $flag;
+
+var_dump($flag);
+
+echo "\n";
