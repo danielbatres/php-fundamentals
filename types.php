058b64deabf9b46b87329c3223e8dd127f7b4f2d
danielbatres <danielbatresuser@gmail.com>
2023-10-20 12:08:39 -0600

    Data type

---
+++ types.php
@@ -0,0 +1,20 @@
+<?php
+
+$number = "24";
+
+$new_number = $number + 2;
+
+var_dump($new_number);
+var_dump($number);
+
+$second_number = 10;
+$second_number += 0.5;
+
+var_dump($second_number);
+
+$potatoes = "10 potatos";
+$potatoes_count = $potatoes + 5;
+
+echo $potatoes_count;
+
+echo "\n";
