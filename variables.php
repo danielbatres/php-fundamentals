75b4a361d56917ceaaec146da8b7d4afead921b3
danielbatres <danielbatresuser@gmail.com>
2023-10-19 21:52:16 -0600

    Variables and constants

---
+++ variables.php
@@ -0,0 +1,16 @@
+<?php 
+
+$number_one = 8;
+$number_two = 7;
+
+define("NUMBER_PI", 3.141592);
+// define("NUMBER_PI", 2); Warning
+
+const NUMBER_E = 2.71828;
+
+echo ($number_one + $number_two) . "\n";
+
+echo NUMBER_PI . "\n";
+echo NUMBER_E . "\n";
+
+echo "\n";
