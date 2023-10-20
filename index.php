fc6404ab1c194d19df1d66ce0b46603ea3c60c7b
danielbatres <danielbatresuser@gmail.com>
2023-10-19 21:28:07 -0600

    Variables and string concatenation with php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "Hello this is my php file";
+echo "Hello this is my php file \n" ;
+
+$name = "Daniel";
+$lastname = "Batres";
+
+echo "My name is " . $name . " " .  $lastname . "\n";
+
+echo "No, my name is $name $lastname \n";
+
+echo "4 x 5 = " . (4 * 5) . "\n";
