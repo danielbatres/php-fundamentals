a8c525269999f1f1097090f95ea7da6cff58beea
danielbatres <danielbatresuser@gmail.com>
2023-10-20 13:39:35 -0600

    Logic operators and arithmetic operators

---
+++ arithmetic-operators.php
@@ -0,0 +1,16 @@
+<?php
+
+$result = 5 ** 3;
+
+echo $result . "\n";
+
+echo (5 * 6) . " " . (80 / 4) . "\n";
+
+echo 5 % 2 . "\n";
+
+$hours = 7201;
+
+echo "$hours seconds to hours is " . ($hours / 3600) . "\n";
+echo "Module " . ($hours % 3600);
+
+echo "\n";
---
+++ logic-operators.php
@@ -0,0 +1,16 @@
+<?php
+
+$feline_cats = true;
+$cats_four_legs = true;
+$cats_fly = false;
+$cats_program_with_php = false;
+
+var_dump($feline_cats && $cats_four_legs);
+var_dump($cats_four_legs || $cats_fly);
+var_dump(!$cats_four_legs);
+
+$result = $cats_four_legs and $cats_program_with_php;
+
+var_dump($result);
+
+echo "\n";
