cb33b823de2dd1595d04699b2d32c639dfea90bf
danielbatres <danielbatresuser@gmail.com>
2023-10-20 12:22:04 -0600

    Type analysis

---
+++ type-analysis.php
@@ -0,0 +1,18 @@
+<?php
+
+// Easy
+$name = "Daniel"; // string
+$last_name = "Batres"; // string
+$age = 18; // int
+$approved = true; // boolean
+
+//Medium
+$average = (8 * 9.5 + 9 + 10 + 8) / 5; // decimal
+$full_name = $name . " " . $last_name; // string
+$took_exam = (bool) 1; // boolean
+
+//Advanced
+$number_questions = 5 + "5"; // int
+$number_answers = "5" + 5; // int
+$max_average = $number_questions / 1.0; // decimal
+$cats = 3 + "5 cats"; // int (with warning)
