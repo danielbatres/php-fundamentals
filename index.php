38bf79ba4ddab43a21ab7bc41abe88583abe536d
danielbatres <danielbatresuser@gmail.com>
2023-10-19 21:39:41 -0600

    debugging and comments

--- index.php
+++ index.php
@@ -10,3 +10,22 @@ echo "My name is " . $name . " " .  $lastname . "\n";
 echo "No, my name is $name $lastname \n";
 
 echo "4 x 5 = " . (4 * 5) . "\n";
+
+$people = [
+  "Carlos" => 22,
+  "Daniel" => 18,
+  "Juan" => 65
+];
+
+// var_dump($people);
+// print_r($people);
+
+// this is a comment
+# this is another comment
+
+/*
+  any comments
+  you need to write
+*/
+
+echo "\n";
